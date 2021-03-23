2157fa5570b1ed2316d3160ddefe04bf3bd8cfbe
Simon Schoelly <sischoel@gmail.com>
2021-03-23 01:09:06 +0100

    Add IMDB datasets

--- src/TUDatasets.jl
+++ src/TUDatasets.jl
@@ -252,6 +252,8 @@ function __init__()
         FingerprintDataset(),
         COLLABDataset(),
         DBLP_v1Dataset(),
+        IMDB_BINARYDataset(),
+        IMDB_MULTIDataset(),
         REDDIT_BINARYDataset(),
         COLORS_3Dataset(),
         QM9Dataset(),
@@ -759,6 +761,36 @@ edge_labels_map(::DBLP_v1Dataset, i) = ("P2P", "P2W", "W2W")[i + 1]
 
 # TODO there is actually a node map with over 40000 entries defined in readme.txt
 node_labels_type(::DBLP_v1Dataset) = Tuple{UInt16}
+
+## --------------------------------------
+##    IMDB-BINARY
+## --------------------------------------
+
+struct IMDB_BINARYDataset <: TUDataset end
+
+dataset_name(::IMDB_BINARYDataset) = "IMDB-BINARY"
+
+dataset_hash(::IMDB_BINARYDataset) = "b291ec8b26d85c70faa2ba0a2433e1f407ed2ef5d0fc072d36b9a95e49a1bb27"
+
+dataset_references(::IMDB_BINARYDataset) = [14]
+
+graph_eltype(::IMDB_BINARYDataset) = Int16
+
+graph_labels_type(::IMDB_BINARYDataset) = @NamedTuple{class::Int8}
+
+## --------------------------------------
+##    IMDB-MULTI
+## --------------------------------------
+
+struct IMDB_MULTIDataset <: TUDataset end
+
+dataset_name(::IMDB_MULTIDataset) = "IMDB-MULTI"
+
+dataset_hash(::IMDB_MULTIDataset) = "a4a302149ebf4c76fa1f0fb108baff89fcbf9d35de306b18f27a8419b9a1a690"
+
+dataset_references(::IMDB_MULTIDataset) = [14]
+
+graph_labels_type(::IMDB_MULTIDataset) = @NamedTuple{class::Int8}
 
 ## --------------------------------------
 ##    REDDIT-BINARY
